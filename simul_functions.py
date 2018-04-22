a376c57a43bd2710481c30b0cb72f45939677b3b
njohnsto <32132609+njohnsto@users.noreply.github.com>
2018-04-22 11:59:33 +0200

    Add files via upload

--- simul_functions.py
+++ simul_functions.py
@@ -24,7 +24,7 @@ def S_i_fit(cos, alpha, beta, f):
     return f*(alpha*x**2+beta*x+1)
 
 
-def get_random_vars(N,theta,S125):
+def get_random_vars(N):
     E0 = 10**15
     E1 = 10**18
     gamma = -2.5
@@ -38,12 +38,49 @@ def get_random_vars(N,theta,S125):
     E = rndm(E0, E1, gamma, N) 
     
     S_i_ref=S_ref(A,B,E)
-    S=S_i(a,b,c,theta,S_i_ref)
+    cos_2=np.random.rand(N)
+    S=S_i(a,b,c,cos_2,S_i_ref)
     data=pd.DataFrame()
     data['E'] = E
     data['S_ref'] = S_i_ref
-    data['cos2'] = theta
+    data['cos2'] = cos_2
     data['S'] = S
+    data['th'] = np.arccos(np.sqrt(data.cos2))
+    data['lgE'] = np.log10(data.E)
+    data['lgS'] = np.log10(data.S)
+    data['lgS_ref'] = np.log10(data.S_ref)
+    data = data.sort_values(['lgS'])
+    data['I'] = 0
+    bins = np.linspace(0, 1, 11, endpoint = True )
+    ind = np.digitize(data['cos2'],bins)
+    groups = data.groupby(ind)
+                      
+    for name, group in groups:
+        values = group['lgS'].apply(lambda x: group[group['lgS']>x].count())
+        data.loc[group.I.index.tolist(), 'I']= values.I 
+        
+    data['sqrt_I']=np.sqrt(data.I)
+    return data
+
+def get_random_vars2(N,theta,S125):
+    E0 = 10**15
+    E1 = 10**18
+    gamma = -2.5
+
+    A = 10**12
+    B = 1.2
+    b=0.919
+    a=-1.13
+    c=1
+
+    E = rndm(E0, E1, gamma, N) 
+    
+    S_i_ref=S_ref(A,B,E)
+    data=pd.DataFrame()
+    data['E'] = E
+    data['S_ref'] = S_i_ref
+    data['cos2'] = np.cos(theta)**2
+    data['S'] = S125
     data['th'] = np.arccos(np.sqrt(data.cos2))
     data['lgE'] = np.log10(data.E)
     data['lgS'] = np.log10(data.S)
