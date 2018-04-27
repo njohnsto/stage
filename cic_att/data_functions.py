5b4c0d67c44a1e9f021cce0e9f90645a1dae8904
Zig Hampel <zhampel@icecube.wisc.edu>
2018-04-27 14:57:42 +0200

    Cleaning up new functionality, and renaming data_funcs

---
+++ cic_att/data_functions.py
@@ -0,0 +1,228 @@
+
+from __future__ import division, print_function
+import numpy as np
+import pandas as pd
+import scipy as sp
+
+import matplotlib.pyplot as plt
+import math
+
+import scipy.optimize as op
+import emcee
+
+from utils import random_pl, get_signal_ref, get_s125
+from likelihood import lnlike, lnprior, lnprob
+
+
+def generate_toy_data(events, minE, maxE, gamma, A, B, alpha, beta, minTheta):
+    """Function that generates simulated data given predefined input
+
+    Parameters
+    ----------
+    events    : int
+                Number of events to simulate
+    minE      : float
+                Minimum energy (eV)
+    maxE      : float
+                Maximum energy (eV)
+    gamma     : float
+                Spectral index
+    A         : float
+                Reference energy (eV)
+    B         : float
+                Relation to s38
+    alpha     : float
+                True attenuation coeff
+    beta      : float
+                True attenuation coeff
+    minTheta  : float
+                Minimum zenith value (degrees)
+
+    Returns
+    -------
+    data : pandas array
+           Pandas array with simulated data
+    """
+    # Random energy samples from power law
+    energy = random_pl(minE, maxE, gamma, events)
+    s38 = get_signal_ref(A, B, energy)
+    cos2 = np.random.uniform(np.cos(math.radians(minTheta))**2, 1, events)
+    s125 = get_s125(cos2, alpha, beta, s38)
+
+    # Define & fill pandas data array object
+    data = pd.DataFrame()
+    data['energy'] = energy
+    data['cos2'] = cos2
+    data['s38'] = s38
+    data['zenith'] = np.arccos(np.sqrt(data.cos2))
+    data['zenith_er'] = np.random.uniform(math.radians(0.5), math.radians(1.5), events)
+    data['s125'] = s125
+    data['s125_error'] = np.abs(np.random.uniform(0.05, 0.1, events) * s125)
+    data['I'] = 0
+
+    return data
+
+
+def set_intensity(data, n_bins):
+    """Function that calculates the intensity for each group
+
+    Parameters
+    ----------
+    data  : pandas array
+           Pandas data array object
+    n_bins : int
+           number of bins to
+
+    Returns
+    -------
+    data : pandas array
+    """
+    # Define cosine squared bins
+    min_cos2 = data.cos2.min()
+    cos2_bins = np.linspace(min_cos2, 1, n_bins, endpoint=True)
+
+    # sort data by S125 values
+    data = data.sort_values(['s125'])
+
+    # Bin and group by cosine ^ 2
+    ind = np.digitize(data['cos2'], cos2_bins)
+    groups = data.groupby(ind)
+
+    for name, group in groups:
+        values = group['s125'].apply(lambda x: group[group['s125'] > x].count())
+        data.loc[group.I.index.tolist(), 'I'] = values.I
+
+    return data
+
+
+def get_data_to_fit(data, intensity, n_bins):
+    """Function that extracts requested data based
+       on intensity, and bins in cosine**2 in
+       accordance with requested input
+
+    Parameters
+    ----------
+    data      : pandas array
+                Pandas data array object
+    intensity : float
+                Requested intensity value
+    n_bins    : int
+                Number of zenith bins
+
+    Returns
+    -------
+    s125_fit        : array (float)
+                      s125 values to fit
+    s125_fit_error  : array (float)
+                      associated uncertainties
+    s38_fit         : array (float)
+                      s38 values to fit
+    bin_centers     : array (float)
+                      centers of cosine**2 bins
+    """
+    # Define centers of bins
+    min_cos2 = data.cos2.min()
+    bins = np.linspace(min_cos2, 1, n_bins, endpoint=True)
+    bin_centers = np.diff(bins)/2+bins[0:-1]
+    # Get data values at intensity
+    val = data.loc[data.I == intensity]
+    s125_fit = np.asarray(val.s125.tolist())
+    s38_fit = np.asarray(val.s38.tolist())
+    s125_fit_error = np.asarray(val.s125_error.tolist())
+    # introduce checks for intensity
+    return (s125_fit, bin_centers, s38_fit, s125_fit_error)
+
+
+def get_attenuation_parameters(s125, cos2):
+    """Function that performs a s125 parametrization fit with scipy
+
+    Parameters
+    ----------
+    s125 : array (float)
+           S125 values
+    cos2 : array (float)
+           associated cosine**2 values
+
+    Returns
+    -------
+    parameters  : list (float)
+                  best fit parameters
+    cov2        : 2darray (float)
+                  covariance matrix
+    """
+    parameters, cov2 = sp.optimize.curve_fit(get_s125, cos2, s125)
+    return (parameters, cov2)
+
+
+def get_attenuation_parameters2(init_params, s125_fit, s38_fit, s125_fit_error, bins):
+    """Function that performs 
+
+    Parameters
+    ----------
+    init_params     : list (float)
+                      initial parameters
+    s125_fit        : array (float)
+                      s125 values to fit
+    s125_fit_error  : array (float)
+                      associated uncertainties
+    s38_fit         : array (float)
+                      s38 values to fit
+    bin_centers     : array (float)
+                      centers of cosine**2 bins
+
+    Returns
+    -------
+    good_samples    : 2darray (float)
+                      MCMC samples having removed 
+                      values outside allowed region
+    """
+
+    # Use as guess
+    a_true = init_params[0]
+    b_true = init_params[1]
+    f_true = init_params[2]
+    y = get_s125(bins, a_true, b_true, f_true)
+
+    # Recenter cosine**2 to 38 deg
+    cos_ref = np.cos(math.radians(38))**2
+    cos2 = bins - cos_ref
+    # Minimize negative log-likelihood function
+    nll = lambda *args: -lnlike(*args)
+    result = sp.optimize.minimize(nll, [a_true, b_true, f_true], args=(cos2, y, s125_fit_error))
+
+    # Prep for emcee
+    ndim, nwalkers = 3, 100
+    # Set starting positions wiggled normally around min nll estimate
+    pos = [result['x'] * np.random.randn(ndim) for i in range(nwalkers)]
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(cos2, y, s125_fit_error))
+
+    # Run emcee, get samples
+    sampler.run_mcmc(pos, 5000)
+    all_samples = sampler.chain[:, 500:, :].reshape((-1, ndim))
+    good_samples = []
+    # Remove burn-in and samples outside of allowed prior bounds
+    for i in range(0, 5000-500):
+        if lnprior(all_samples[i]) != -np.inf:
+            good_samples.append(all_samples[i])
+    good_samples = np.asarray(good_samples)
+    print("Life is amazing")
+
+    return good_samples
+
+
+def get_bootstrap_data(data):
+    """Function that wiggles s125 by assoc uncertainties
+
+    Parameters
+    ----------
+    data  : pandas array
+            Pandas data array object
+
+    Returns
+    -------
+    data : pandas array
+            Pandas data array object
+    """
+    new_data = data.copy()
+    new_data.s125 = np.random.normal(data.s125.tolist(), data.s125_error.tolist())
+    return new_data
--- cic_att/utils.py
+++ cic_att/utils.py
@@ -44,14 +44,21 @@ def get_signal_ref(e_ref, B, energy):
 
     Parameters
     ----------
-    e_ref  : float
-           Reference energy
-    B      : float
-           Relational scale to s38
-    energy : array (float)
-           Energy values at which to get reference signal
+    e_ref   : float
+            Reference energy
+    B       : float
+            Relational scale to s38
+    energy  : array (float)
+            Energy values at which to get reference signal
+
+    Returns
+    -------
+    sig_ref : array (float)
+             Scaled reference signal values
     """
-    return (energy / e_ref)**(1 / B)
+    sig_ref = (energy / e_ref)**(1 / B)
+
+    return sig_ref
 
 
 def get_s125(cos2, alpha, beta, ref_signal):
@@ -60,17 +67,24 @@ def get_s125(cos2, alpha, beta, ref_signal):
     Parameters
     ----------
     cos2       : array (float)
-           Values of cos^2
+                Values of cos^2
     alpha      : float
-           Linear coefficient of S125 parametrization
+                Linear coefficient of S125 parametrization
     beta       : float
-           Quadratic coefficient of S125 parametrization
-    signal_ref : array (float)
-           Reference signal value, s38 (S at 38 deg zenith)
+                Quadratic coefficient of S125 parametrization
+    ref_signal : array (float)
+                Reference signal value, s38 (S at 38 deg zenith)
+
+    Returns
+    -------
+    s_125      : array (float)
+                Signal at 125 parametrized
     """
 
     # Center cosine **2 values on cosine(38)**2
     cos2_ref = np.cos(math.radians(38))**2
     x = cos2 - cos2_ref
 
-    return ref_signal * (1 + alpha*x + beta*x**2)
+    s_125 = ref_signal * (1 + alpha*x + beta*x**2)
+
+    return s_125
