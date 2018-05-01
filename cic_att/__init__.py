f058e9af5320374e2d587726d3b872ab1ab131f5
Zig Hampel <zhampel@icecube.wisc.edu>
2018-05-01 10:28:56 +0200

    Adding version and init file

---
+++ cic_att/__init__.py
@@ -0,0 +1,9 @@
+
+from .__version__ import __version__
+
+from .data_functions import obtain_attenuation
+from .data_functions import merge_and_select_data
+from .data_functions import get_bootstrap_data
+from .data_functions import get_attenuation_parameters2
+from .data_functions import generate_toy_data
+from .data_functions import set_intensity
